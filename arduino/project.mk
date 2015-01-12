e809f375c93cdb9b5fd98e88daea512d11f55a4a
Russell Hay <me@russellhay.com>
2015-01-11 20:50:51 -0800

    Really basic command line arduino build based on https://github.com/ladislas/Bare-Arduino-Project

---
+++ arduino/project.mk
@@ -0,0 +1,22 @@
+THIS_MAKEFILE_PATH:=$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
+THIS_DIR:=$(shell cd $(dir $(THIS_MAKEFILE_PATH));pwd)
+PROJECT_DIR = $(THIS_DIR)
+USER_LIB_PATH = $(PROJECT_DIR)/lib
+CFLAGS_STD = -std=gnu11
+CXXFLAGS_STD = -std=gnu++11
+CXXFLAGS = -pedantic -Wall -Wextra
+CXXFLAGS += -fdiagnostics-color
+
+ARDUINO_DIR = /Applications/ArduinoYun.app/Contents/Resources/Java
+ARDMK_DIR = /usr/local/Cellar/arduino-mk/HEAD
+AVR_TOOLS_DIR = /usr/local
+MONITOR_PORT = /dev/ttyACM0
+BOARD_TAG = uno
+ARCHITECTURE = avr
+BOARD_SUB = atmega168
+ARDUINO_LIBS = 
+
+CURRENT_DIR = $(shell basename $(CURDIR))
+OBJDIR = $(PROJECT_DIR)/build/$(BOARD_TAG)/$(CURRENT_DIR)
+
+include /usr/local/opt/arduino-mk/Arduino.mk
---
+++ arduino/src/ledvm/test.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+void setup() {
+	Serial.begin(115200);
+	delay(1000);
+}
+void loop() {
+	delay(1000);
+}
